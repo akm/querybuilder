0370f7c8ed708d8580910db10f048b0bb1fc728f
akm <akm2000@gmail.com>
2019-04-20 15:40:26 +0900

    :recycle: Extract assign.go

---
+++ assign.go
@@ -0,0 +1,32 @@
+package querybuilder
+
+import (
+	"fmt"
+	"reflect"
+)
+
+type AssignFunc func(interface{})
+type AssignFuncs []AssignFunc
+
+func (s AssignFuncs) Assign(entity interface{}) {
+	fmt.Printf("entity %v\n", entity)
+	for _, f := range s {
+		f(entity)
+	}
+}
+
+func (s AssignFuncs) AssignAll(entities interface{}) error {
+	v := reflect.ValueOf(entities)
+	switch v.Type().Kind() {
+	case reflect.Slice:
+		l := v.Len()
+		for i := 0; i < l; i++ {
+			s.Assign(v.Index(i).Interface())
+		}
+	case reflect.Ptr:
+		return s.AssignAll(v.Elem().Interface())
+	default:
+		return fmt.Errorf("Unsupported type of slice %T", entities)
+	}
+	return nil
+}
