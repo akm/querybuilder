b1ae90d360c85d2ba18285a86d457fa649b247c8
akm <akm2000@gmail.com>
2019-04-22 17:10:02 +0900

    :+1: Nested field namesupport like field1.field2.field3

--- assign.go
+++ assign.go
@@ -3,6 +3,7 @@ package querybuilder
 import (
 	"fmt"
 	"reflect"
+	"strings"
 )
 
 type AssignFunc func(interface{}) error
@@ -42,18 +43,51 @@ func AssignFuncFor(field string, value interface{}) AssignFunc {
 		if e.Type().Kind() == reflect.Ptr {
 			e = e.Elem()
 		}
+		v := reflect.ValueOf(value)
 		switch e.Type().Kind() {
 		case reflect.Struct:
-			v := reflect.ValueOf(value)
-			f := e.FieldByName(field)
-			if f.IsValid() {
+			err := ReflectWalkIn(&e, field, ".", func(f *reflect.Value) error {
 				f.Set(v)
 				return nil
-			} else {
-				return fmt.Errorf("Entity type: %T Field: %s not found. %v", entity, field, entity)
+			})
+			if err != nil {
+				return err
 			}
+			return nil
 		default:
 			return fmt.Errorf("Entity type: %T is not a struct. %v", entity, entity)
 		}
 	}
+}
+
+func ReflectWalkIn(base *reflect.Value, field, sep string, f func(*reflect.Value) error) error {
+	fields := strings.Split(field, sep)
+	return ReflectWalkInImpl(base, fields, f)
+}
+
+func ReflectWalkInImpl(curr *reflect.Value, fields []string, f func(*reflect.Value) error) error {
+	if len(fields) < 1 {
+		return f(curr)
+	}
+
+	switch curr.Type().Kind() {
+	case reflect.Slice, reflect.Array:
+		l := curr.Len()
+		for i := 0; i < l; i++ {
+			v := curr.Index(i)
+			err := ReflectWalkInImpl(&v, fields, f)
+			if err != nil {
+				return err
+			}
+		}
+	case reflect.Struct:
+		field := curr.FieldByName(fields[0])
+		if !field.IsValid() {
+			return fmt.Errorf("%s has no field named %s (from Entity %s %v)", curr.String(), fields[0])
+		}
+		return ReflectWalkInImpl(&field, fields[1:], f)
+	default:
+		return fmt.Errorf("%s is not struct but %v", curr.String(), curr.Interface())
+	}
+	return nil
 }
--- builder_test.go
+++ builder_test.go
@@ -28,6 +28,8 @@ type Entity4Test struct {
 	EnumA EnumA
 }
 
+const Kind4Test = "entity4test"
+
 var Entities = []*Entity4Test{
 	{Int1: 1, Int2: 1, Str1: "a", Str2: "foo", EnumA: EnumA1},
 	{Int1: 2, Int2: 1, Str1: "b", Str2: "bar", EnumA: EnumA2},
@@ -36,8 +38,6 @@ var Entities = []*Entity4Test{
 	{Int1: 5, Int2: 5, Str1: "e", Str2: "quux", EnumA: EnumA2},
 	{Int1: 6, Int2: 8, Str1: "f", Str2: "corge", EnumA: EnumA3},
 }
-
-const Kind4Test = "entity4test"
 
 func TestBuilder(t *testing.T) {
 	testsupport.WithAEContext(t, func(ctx context.Context) error {
@@ -164,6 +164,148 @@ func TestBuilder(t *testing.T) {
 				int1s = append(int1s, entity.Int1)
 			}
 			assert.Equal(t, []int{3, 4, 5}, int1s)
+		}
+
+		return nil
+	})
+}
+
+type SubEntity struct {
+	I1 int
+	S1 string
+}
+
+type ComplicatedEntity4Test struct {
+	ID      int
+	Name    string
+	Strings []string
+	Ints    []int
+	Sub1    SubEntity
+	Subs    []SubEntity
+}
+
+const ComplicatedKind4Test = "complicated4test"
+
+var ComplicatedEntities = []*ComplicatedEntity4Test{
+	{ID: 1, Name: "Foo"},
+	{ID: 2, Name: "Bar", Strings: []string{"a"}, Ints: []int{1, 2, 3}},
+	{ID: 3, Name: "Baz", Strings: []string{"a", "b"}, Ints: []int{2}},
+	{ID: 4, Name: "Qux", Strings: []string{"d"}, Sub1: SubEntity{I1: 1, S1: "A"}},
+	{ID: 5, Name: "Quux", Strings: []string{"d"}, Subs: []SubEntity{{I1: 1, S1: "A"}, {I1: 3, S1: "C"}}},
+	{ID: 6, Name: "Corge", Strings: []string{"b"}, Subs: []SubEntity{{I1: 2, S1: "B"}, {I1: 3, S1: "C"}}},
+	{ID: 7, Name: "Grault", Strings: []string{"c"}, Subs: []SubEntity{{I1: 2, S1: "C"}, {I1: 4, S1: "D"}}},
+	{ID: 8, Name: "Garply", Strings: []string{"d"}, Sub1: SubEntity{I1: 2, S1: "B"}},
+	{ID: 9, Name: "Waldo", Strings: []string{"e"}, Sub1: SubEntity{I1: 2, S1: "B"}},
+}
+
+func TestBuilderWithComplicatedEntities(t *testing.T) {
+	testsupport.WithAEContext(t, func(ctx context.Context) error {
+		{
+			keys := make([]*datastore.Key, len(ComplicatedEntities))
+			for i, _ := range keys {
+				keys[i] = datastore.NewIncompleteKey(ctx, ComplicatedKind4Test, nil)
+			}
+			_, err := datastore.PutMulti(ctx, keys, ComplicatedEntities)
+			assert.NoError(t, err)
+		}
+
+		{
+			queryValue := 2
+			b := New("ID", "Name", "Sub1.I1", "Sub1.S1")
+			b.Eq("Sub1.I1", queryValue)
+			b.Asc("ID")
+			assert.Equal(t, Strings{"ID", "Name", "Sub1.S1"}, b.ProjectFields())
+			q, f := b.Build(datastore.NewQuery(ComplicatedKind4Test))
+			var entities []*ComplicatedEntity4Test
+			_, err := q.GetAll(ctx, &entities)
+			assert.NoError(t, err)
+
+			type pattern struct {
+				ID   int
+				Name string
+				SubI int
+				SubS string
+			}
+
+			patterns1 := []pattern{
+				{8, "Garply", 0, "B"},
+				{9, "Waldo", 0, "B"},
+			}
+			patterns2 := []pattern{
+				{8, "Garply", queryValue, "B"},
+				{9, "Waldo", queryValue, "B"},
+			}
+			impl := func(patterns []pattern) error {
+				if assert.Equal(t, len(patterns), len(entities)) {
+					for i, ptn := range patterns {
+						e := entities[i]
+						assert.Equal(t, ptn.ID, e.ID)
+						assert.Equal(t, ptn.Name, e.Name)
+						assert.Equal(t, ptn.SubI, e.Sub1.I1)
+						assert.Equal(t, ptn.SubS, e.Sub1.S1)
+					}
+				}
+				return nil
+			}
+
+			impl(patterns1)
+			f.AssignAll(&entities)
+			impl(patterns2)
+		}
+
+		{
+			queryValue := 3
+			b := New("ID", "Name", "Subs.I1", "Subs.S1")
+			b.Eq("Subs.I1", queryValue)
+			b.Asc("ID")
+			assert.Equal(t, Strings{"ID", "Name", "Subs.S1"}, b.ProjectFields())
+			q, f := b.Build(datastore.NewQuery(ComplicatedKind4Test))
+			var entities []*ComplicatedEntity4Test
+			_, err := q.GetAll(ctx, &entities)
+			assert.NoError(t, err)
+
+			type pattern struct {
+				ID   int
+				Name string
+				SubI int
+				SubS string
+			}
+
+			patterns1 := []pattern{
+				{5, "Quux", 0, "A"}, // This SubI data must be set but datastore doesn't set it
+				{5, "Quux", 0, "C"},
+				{6, "Corge", 0, "B"}, // This SubI data must be set but datastore doesn't set it
+				{6, "Corge", 0, "C"},
+			}
+
+			patterns2 := []pattern{
+				{5, "Quux", queryValue, "A"}, // This SubI is not value set with SubS "A". It must be 1.
+				{5, "Quux", queryValue, "C"},
+				{6, "Corge", queryValue, "B"}, // This SubI is not value set with SubS "B". It must be 2.
+				{6, "Corge", queryValue, "C"},
+			}
+
+			impl := func(patterns []pattern) error {
+				if assert.Equal(t, len(patterns), len(entities)) {
+					for i, ptn := range patterns {
+						e := entities[i]
+						assert.Equal(t, ptn.ID, e.ID)
+						assert.Equal(t, ptn.Name, e.Name)
+						if assert.Equal(t, 1, len(e.Subs)) {
+							{
+								s := e.Subs[0]
+								assert.Equal(t, ptn.SubI, s.I1)
+								assert.Equal(t, ptn.SubS, s.S1)
+							}
+						}
+					}
+				}
+				return nil
+			}
+
+			impl(patterns1)
+			f.AssignAll(&entities)
+			impl(patterns2)
 		}
 
 		return nil
