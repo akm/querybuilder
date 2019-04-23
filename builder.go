0b5e357834449a0946a7d3c66923df210b7b7a2b
akm <akm2000@gmail.com>
2019-04-23 16:55:45 +0900

    :recycle: Extract valued_filter.go from builder.go

--- builder.go
+++ builder.go
@@ -4,22 +4,6 @@ import (
 	"google.golang.org/appengine/datastore"
 )
 
-type ValuedFilter struct {
-	Name     string `json:"name"`
-	IntValue int    `json:"value"`
-}
-
-func (vf *ValuedFilter) Call(q *datastore.Query) *datastore.Query {
-	switch vf.Name {
-	case "offset":
-		return q.Offset(vf.IntValue)
-	case "limit":
-		return q.Limit(vf.IntValue)
-	default:
-		return q
-	}
-}
-
 type QueryBuilder struct {
 	Fields     Strings
 	ignored    Strings
