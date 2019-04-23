010116f96f0c4f578663ea2530f98b1ef4fe003d
akm <akm2000@gmail.com>
2019-04-23 16:54:25 +0900

    :recycle: Extract condition.go from builder.go

--- builder.go
+++ builder.go
@@ -4,16 +4,6 @@ import (
 	"google.golang.org/appengine/datastore"
 )
 
-type Condition struct {
-	Field string      `json:"field"`
-	Ope   Ope         `json:"ope"`
-	Value interface{} `json:"value"`
-}
-
-func (c *Condition) Call(q *datastore.Query) *datastore.Query {
-	return q.Filter(c.Field+c.Ope.String(), c.Value)
-}
-
 type ValuedFilter struct {
 	Name     string `json:"name"`
 	IntValue int    `json:"value"`
